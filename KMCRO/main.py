ff0591d79f65a09d6314060bcd1ed3bc1250d10d
Serfeg <70938016+Serfeg@users.noreply.github.com>
2022-02-02 18:05:52 +0700

    4. Готовый алгоритм К-средних

--- KMCRO/main.py
+++ KMCRO/main.py
@@ -1,7 +1,7 @@
 import math as m
 import random as r
 
-import matplotlib as plot
+import matplotlib.pyplot as plt
 
 
 def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
@@ -17,7 +17,7 @@ def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
 def euclideanDistance(dataSetRow, centroid):
     sum = 0
     for i in range(len(dataSetRow)):
-        sum += (dataSetRow[i]-centroid[i])**2
+        sum += (dataSetRow[i] - centroid[i]) ** 2
     return sum
 
 
@@ -27,26 +27,52 @@ def countSse(euclidDist):
         sum += min(euclidDist[i])
     return sum
 
-n = 2
 
-dataSet = [[5, 0],
-           [5, 2],
-           [3, 1],
-           [0, 4],
-           [2, 1],
-           [4, 2],
-           [2, 2],
-           [2, 3],
-           [1, 3],
-           [5, 4]]
+n = 3
+
+#dataSet = [[5, 0],
+           #[5, 2],
+           #[3, 1],
+           #[0, 4],
+           #[2, 1],
+           #[4, 2],
+           #[2, 2],
+           #[2, 3],
+           #[1, 3],
+           #[5, 4]]
+
+dataSet = [[r.randint(-10, 10) for i in range(2)] for j in range(100)]
 lenDs = len(dataSet)
-#cluster = [r.randint(1,n) for i in range(lenDs)]
-cluster = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+cluster = [r.randint(1, n) for i in range(lenDs)]
+#cluster = [1, 2, 1, 2, 1, 2, 2, 2, 1, 2]
 
+exits = True
+k = 1
+while exits:
+    centroid = [[findCentroid(dataSet, col, cluster, kNumber + 1) for col in range(len(dataSet[0]))] for kNumber in
+                range(n)]
+    euclidDist = [[euclideanDistance(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in range(lenDs)]
+    newCluster = [euclidDist[i].index(min(euclidDist[i])) + 1 for i in range(len(euclidDist))]
+    sse = countSse(euclidDist)
+    if cluster == newCluster:
+        exits = False
+    else:
+        cluster = newCluster
+        k += 1
 
-centroid = [[findCentroid(dataSet, col, cluster, kNumber+1) for col in range(len(dataSet[0]))] for kNumber in range(n)]
-euclidDist = [[euclideanDistance(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in range(lenDs)]
-newClust = [euclidDist[i].index(min(euclidDist[i]))+1 for i in range(len(euclidDist))]
-sse = countSse(euclidDist)
-print(f"Dataset: {dataSet}\nCount of Cluster: {n}\nRandom Cluster: {cluster}\nCentroid: {centroid}\nNew Cluster: {newClust}\nSSE: {sse}")
+print(f"Dataset: {dataSet}\nCount of Cluster: {n}\nCentroid: {centroid}"
+      f"\nFinal Cluster: {newCluster}\nSSE: {sse}\nCount of Iteration: {k}")
 
+for i in range(lenDs):
+    if newCluster[i] == 1:
+        plt.scatter(dataSet[i][0],dataSet[i][1], c='red', marker='o', linewidths=5)
+    elif newCluster[i] == 2:
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='blue', marker='^', linewidths=5)
+    elif newCluster[i] == 3:
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='green', marker='s', linewidths=5)
+
+for i in range(len(centroid)):
+    plt.scatter(centroid[i][0], centroid[i][1], c='black', marker='2', linewidths=5)
+
+plt.grid(True)
+plt.show()
