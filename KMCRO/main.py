3f87a155f04da1cb5a805451b96402bdf15b3fad
Serfeg <70938016+Serfeg@users.noreply.github.com>
2022-02-02 16:39:50 +0700

    3. Почти готовый алгоритм

--- KMCRO/main.py
+++ KMCRO/main.py
@@ -4,40 +4,49 @@ import random as r
 import matplotlib as plot
 
 
-def randomCluster(dataSet, n):
-    for i in range(len(dataSet)):
-        dataSet[i][-1] = r.randint(1, n)
-    return dataSet
-
-
-def findCentroid(dataSet, C, col):
+def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
     sum = 0
     z = 0
     for i in range(len(dataSet)):
-        if dataSet[i][-1] == C:
+        if cluster[i] == kNumber:
             z += 1
-            sum += dataSet[i][col]
+            sum += dataSet[i][dataSetColumn]
     return sum / z
 
 
+def euclideanDistance(dataSetRow, centroid):
+    sum = 0
+    for i in range(len(dataSetRow)):
+        sum += (dataSetRow[i]-centroid[i])**2
+    return sum
+
+
+def countSse(euclidDist):
+    sum = 0
+    for i in range(len(euclidDist)):
+        sum += min(euclidDist[i])
+    return sum
+
 n = 2
 
-dataSet = [[5, 0, 1],
-           [5, 2, 2],
-           [3, 1, 1],
-           [0, 4, 2],
-           [2, 1, 1],
-           [4, 2, 2],
-           [2, 2, 1],
-           [2, 3, 2],
-           [1, 3, 1],
-           [5, 4, 2]]
+dataSet = [[5, 0],
+           [5, 2],
+           [3, 1],
+           [0, 4],
+           [2, 1],
+           [4, 2],
+           [2, 2],
+           [2, 3],
+           [1, 3],
+           [5, 4]]
+lenDs = len(dataSet)
+#cluster = [r.randint(1,n) for i in range(lenDs)]
+cluster = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
 
-Centroid = [[findCentroid(dataSet, j+1, i) for i in range(len(dataSet[0])-1)] for j in range(n)]
 
+centroid = [[findCentroid(dataSet, col, cluster, kNumber+1) for col in range(len(dataSet[0]))] for kNumber in range(n)]
+euclidDist = [[euclideanDistance(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in range(lenDs)]
+newClust = [euclidDist[i].index(min(euclidDist[i]))+1 for i in range(len(euclidDist))]
+sse = countSse(euclidDist)
+print(f"Dataset: {dataSet}\nCount of Cluster: {n}\nRandom Cluster: {cluster}\nCentroid: {centroid}\nNew Cluster: {newClust}\nSSE: {sse}")
 
-# x = [5, 5, 3, 0, 2, 4, 2, 2, 1, 5]
-# y = [0, 2, 1, 4, 1, 2, 2, 3, 3, 4]
-# n = 2
-# K = [r.randint(1,n) for i in range(len(x))]
-# K = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
