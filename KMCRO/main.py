c6d7ceb52ed9874f49c0ceef7b0187ddff0e0e34
Serfeg <70938016+Serfeg@users.noreply.github.com>
2022-02-03 13:41:26 +0700

    5.1 Комментарии

--- KMCRO/main.py
+++ KMCRO/main.py
@@ -4,6 +4,7 @@ import random as r
 import matplotlib.pyplot as plt
 
 
+#Расчёт центроид
 def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
     sum = 0
     z = 0
@@ -11,9 +12,12 @@ def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
         if cluster[i] == kNumber:
             z += 1
             sum += dataSet[i][dataSetColumn]
+    if z == 0:
+        z = 1
     return sum / z
 
 
+#Расчёт евклидова расстояния
 def euclideanDistance(dataSetRow, centroid):
     sum = 0
     for i in range(len(dataSetRow)):
@@ -21,6 +25,7 @@ def euclideanDistance(dataSetRow, centroid):
     return sum
 
 
+#Расчёт квадратичной ошибки
 def countSse(euclidDist):
     sum = 0
     for i in range(len(euclidDist)):
@@ -28,8 +33,10 @@ def countSse(euclidDist):
     return sum
 
 
+#Количество кластеров
 n = 5
 
+#Пример из excel файла
 #dataSet = [[5, 0],
            #[5, 2],
            #[3, 1],
@@ -41,15 +48,18 @@ n = 5
            #[1, 3],
            #[5, 4]]
 
+#Рандомный датасет на 1000 точек
 dataSet = [[r.randint(-15, 15) for i in range(2)] for j in range(1000)]
 lenDs = len(dataSet)
+
+#Рандомный кластер на длину датасета
 cluster = [r.randint(1, n) for i in range(lenDs)]
 #cluster = [1, 2, 1, 2, 1, 2, 2, 2, 1, 2]
 
 exits = True
 k = 1
 while exits:
-    centroid = [[findCentroid(dataSet, col, cluster, kNumber + 1) for col in range(len(dataSet[0]))] for kNumber in
+    centroid = [[findCentroid(dataSet, col, cluster, kNumber + 1) for col in range(2)] for kNumber in
                 range(n)]
     euclidDist = [[euclideanDistance(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in range(lenDs)]
     newCluster = [euclidDist[i].index(min(euclidDist[i])) + 1 for i in range(len(euclidDist))]
@@ -64,18 +74,18 @@ print(f"Count of Cluster: {n}\nCentroid: {centroid}\nSSE: {sse}\nCount of Iterat
 
 for i in range(lenDs):
     if newCluster[i] == 1:
-        plt.scatter(dataSet[i][0], dataSet[i][1], c='red', marker='o', linewidths=5)
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='red', marker='o')
     elif newCluster[i] == 2:
-        plt.scatter(dataSet[i][0], dataSet[i][1], c='blue', marker='^', linewidths=5)
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='blue', marker='^')
     elif newCluster[i] == 3:
-        plt.scatter(dataSet[i][0], dataSet[i][1], c='green', marker='s', linewidths=5)
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='green', marker='s')
     elif newCluster[i] == 4:
-        plt.scatter(dataSet[i][0], dataSet[i][1], c='yellow', marker='p', linewidths=5)
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='pink', marker='p')
     elif newCluster[i] == 5:
-        plt.scatter(dataSet[i][0], dataSet[i][1], c='orange', marker='d', linewidths=5)
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='orange', marker='d')
 
 for i in range(len(centroid)):
     plt.scatter(centroid[i][0], centroid[i][1], c='black', marker='2', linewidths=5)
 
-#plt.grid(True)
+plt.grid(True)
 plt.show()
