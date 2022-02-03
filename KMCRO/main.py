8ae83c877babd49704ac865d79d0400fc471f300
Serfeg <70938016+Serfeg@users.noreply.github.com>
2022-02-03 12:02:53 +0700

    5. Рандомное заполнение датасета

--- KMCRO/main.py
+++ KMCRO/main.py
@@ -28,7 +28,7 @@ def countSse(euclidDist):
     return sum
 
 
-n = 3
+n = 5
 
 #dataSet = [[5, 0],
            #[5, 2],
@@ -41,7 +41,7 @@ n = 3
            #[1, 3],
            #[5, 4]]
 
-dataSet = [[r.randint(-10, 10) for i in range(2)] for j in range(100)]
+dataSet = [[r.randint(-15, 15) for i in range(2)] for j in range(1000)]
 lenDs = len(dataSet)
 cluster = [r.randint(1, n) for i in range(lenDs)]
 #cluster = [1, 2, 1, 2, 1, 2, 2, 2, 1, 2]
@@ -60,19 +60,22 @@ while exits:
         cluster = newCluster
         k += 1
 
-print(f"Dataset: {dataSet}\nCount of Cluster: {n}\nCentroid: {centroid}"
-      f"\nFinal Cluster: {newCluster}\nSSE: {sse}\nCount of Iteration: {k}")
+print(f"Count of Cluster: {n}\nCentroid: {centroid}\nSSE: {sse}\nCount of Iteration: {k}")
 
 for i in range(lenDs):
     if newCluster[i] == 1:
-        plt.scatter(dataSet[i][0],dataSet[i][1], c='red', marker='o', linewidths=5)
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='red', marker='o', linewidths=5)
     elif newCluster[i] == 2:
         plt.scatter(dataSet[i][0], dataSet[i][1], c='blue', marker='^', linewidths=5)
     elif newCluster[i] == 3:
         plt.scatter(dataSet[i][0], dataSet[i][1], c='green', marker='s', linewidths=5)
+    elif newCluster[i] == 4:
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='yellow', marker='p', linewidths=5)
+    elif newCluster[i] == 5:
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='orange', marker='d', linewidths=5)
 
 for i in range(len(centroid)):
     plt.scatter(centroid[i][0], centroid[i][1], c='black', marker='2', linewidths=5)
 
-plt.grid(True)
+#plt.grid(True)
 plt.show()
