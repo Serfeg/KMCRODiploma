2e991c7e4d2eea0953e33f68ef6953e7d2373a5a
Serfeg <70938016+Serfeg@users.noreply.github.com>
2022-02-03 16:22:49 +0700

    5.2 Обновление кода для читабельности

--- KMCRO/main.py
+++ KMCRO/main.py
@@ -4,7 +4,7 @@ import random as r
 import matplotlib.pyplot as plt
 
 
-#Расчёт центроид
+# Расчёт центроид
 def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
     sum = 0
     z = 0
@@ -12,12 +12,10 @@ def findCentroid(dataSet, dataSetColumn, cluster, kNumber):
         if cluster[i] == kNumber:
             z += 1
             sum += dataSet[i][dataSetColumn]
-    if z == 0:
-        z = 1
-    return sum / z
+    return sum / z if z != 0 else 0
 
 
-#Расчёт евклидова расстояния
+# Расчёт евклидова расстояния
 def euclideanDistance(dataSetRow, centroid):
     sum = 0
     for i in range(len(dataSetRow)):
@@ -25,7 +23,7 @@ def euclideanDistance(dataSetRow, centroid):
     return sum
 
 
-#Расчёт квадратичной ошибки
+# Расчёт квадратичной ошибки
 def countSse(euclidDist):
     sum = 0
     for i in range(len(euclidDist)):
@@ -33,34 +31,33 @@ def countSse(euclidDist):
     return sum
 
 
-#Количество кластеров
-n = 5
+# Количество кластеров
+n = 3
 
-#Пример из excel файла
-#dataSet = [[5, 0],
-           #[5, 2],
-           #[3, 1],
-           #[0, 4],
-           #[2, 1],
-           #[4, 2],
-           #[2, 2],
-           #[2, 3],
-           #[1, 3],
-           #[5, 4]]
+# Пример из excel файла
+# dataSet = [[5, 0],
+# [5, 2],
+# [3, 1],
+# [0, 4],
+# [2, 1],
+# [4, 2],
+# [2, 2],
+# [2, 3],
+# [1, 3],
+# [5, 4]]
 
-#Рандомный датасет на 1000 точек
-dataSet = [[r.randint(-15, 15) for i in range(2)] for j in range(1000)]
+# Рандомный датасет на 1000 точек
+dataSet = [[r.randint(-15, 15) for i in range(2)] for j in range(100)]
 lenDs = len(dataSet)
 
-#Рандомный кластер на длину датасета
+# Рандомный кластер на длину датасета
 cluster = [r.randint(1, n) for i in range(lenDs)]
-#cluster = [1, 2, 1, 2, 1, 2, 2, 2, 1, 2]
+# cluster = [1, 2, 1, 2, 1, 2, 2, 2, 1, 2]
 
 exits = True
 k = 1
 while exits:
-    centroid = [[findCentroid(dataSet, col, cluster, kNumber + 1) for col in range(2)] for kNumber in
-                range(n)]
+    centroid = [[findCentroid(dataSet, col, cluster, kNumber + 1) for col in range(2)] for kNumber in range(n)]
     euclidDist = [[euclideanDistance(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in range(lenDs)]
     newCluster = [euclidDist[i].index(min(euclidDist[i])) + 1 for i in range(len(euclidDist))]
     sse = countSse(euclidDist)
@@ -83,6 +80,10 @@ for i in range(lenDs):
         plt.scatter(dataSet[i][0], dataSet[i][1], c='pink', marker='p')
     elif newCluster[i] == 5:
         plt.scatter(dataSet[i][0], dataSet[i][1], c='orange', marker='d')
+    elif newCluster[i] == 6:
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='purple', marker='*')
+    elif newCluster[i] == 7:
+        plt.scatter(dataSet[i][0], dataSet[i][1], c='lime', marker='H')
 
 for i in range(len(centroid)):
     plt.scatter(centroid[i][0], centroid[i][1], c='black', marker='2', linewidths=5)
