befaf5cea9fb148de4210faa77076518971684a2
Serfeg <70938016+Serfeg@users.noreply.github.com>
2022-02-26 14:49:15 +0700

    18.1 Добавление Евклидовой Метрики

--- KMCRO/analysis.py
+++ KMCRO/analysis.py
@@ -35,18 +35,24 @@ f.write('\n\n')
 
 sse = 0
 h = 0
-f.write('Cos+dist\n')
+
+allOriginalCentroids = []
+allChangedCentroid = []
 while h < 5:
     f.write('Итерация №' + str(h + 1) + '\n')
     f.write('Начальные центроиды\n')
     centroid = [[r.randint(-10, 10) for j in range(len(dataSet[0]))] for i in range(k)]
-    for i in range(len(centroid)):
+    cluster = [i for i in originalCluster]
+    allOriginalCentroids.append(centroid)
+
+    for i in range(len(allOriginalCentroids[h])):
         f.write('C' + str(i + 1) + '\t')
-        for j in range(len(centroid[i])):
-            f.write(str(centroid[i][j]) + '\t')
+        for j in range(len(allOriginalCentroids[h][i])):
+            f.write(str(allOriginalCentroids[h][i][j]) + '\t')
         f.write('\n')
     f.write('\n')
 
+    f.write('CosineSimilarity\n')
     while True:
         cosSim = [[kmeans.cosineSimilarity(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in
                   range(len(dataSet))]
@@ -57,9 +63,8 @@ while h < 5:
             break
         else:
             originalCluster = newCluster
-            centroid = [[kmeans.findCentroid(dataSet, col, originalCluster, i + 1) for col in range(len(dataSet[0]))]
-                        for i in
-                        range(k)]
+            centroid = [[kmeans.findCentroid(dataSet, col, cluster, i + 1) for col in range(len(dataSet[0]))]
+                for i in range(k)]
 
     f.write('Итоговые центроиды\n')
     for i in range(len(centroid)):
@@ -70,57 +75,86 @@ while h < 5:
     f.write('\n')
 
     f.write('SSE: ' + str(sse) + '\n\n')
-    fitness = kmeans.fitnessCosWithDist(dataSet, originalCluster, centroid, k)
-    f.write('Fitness: ' + str(fitness) + '\n')
-    countIterCRO = 0
+
+    centroid = [[allOriginalCentroids[h][i][j] for j in range(len(allOriginalCentroids[h][i]))] for i in range(k)]
+    cluster = [i for i in originalCluster]
+    f.write('EuclidDist\n')
     while True:
-        countIterCRO += 1
-        croFitness = []
-        croList = []
+        euclidDist = [[kmeans.euclideanDistance(dataSet[j], centroid[i]) for i in range(len(centroid))] for j in
+                      range(len(dataSet))]
+        newCluster = [euclidDist[i].index(min(euclidDist[i])) + 1 for i in range(len(euclidDist))]
 
-        # singleMoleculeCollision
-        cluster1 = cro.singleMoleculeCollision(len(dataSet), k)
-        croFitness.append(kmeans.fitnessCosWithDist(dataSet, cluster1, centroid, k))
-        croList.append(cluster1)
-
-        # singleMoleculeDecomposition
-        clusterOdd, clusterEven = cro.singleMoleculeDecomposition(len(dataSet), k)
-        fitnessOdd = kmeans.fitnessCosWithDist(dataSet, clusterOdd, centroid, k)
-        fitnessEven = kmeans.fitnessCosWithDist(dataSet, clusterEven, centroid, k)
-        if fitnessOdd >= fitnessEven:
-            croFitness.append(fitnessOdd)
-            croList.append(clusterOdd)
-        else:
-            croFitness.append(fitnessEven)
-            croList.append(clusterEven)
-
-        # intermolecularCollision
-        clusterPhi1, clusterPhi2 = cro.intermolecularCollision(len(dataSet), k)
-        fitnessPhi1 = kmeans.fitnessCosWithDist(dataSet, clusterPhi1, centroid, k)
-        fitnessPhi2 = kmeans.fitnessCosWithDist(dataSet, clusterPhi2, centroid, k)
-        if fitnessPhi1 >= fitnessPhi2:
-            croFitness.append(fitnessPhi1)
-            croList.append(clusterPhi1)
-        else:
-            croFitness.append(fitnessPhi2)
-            croList.append(clusterPhi2)
-
-        # intermolecularSynthesis
-        cluster2 = cro.intermolecularSynthesis(len(dataSet), k)
-        croFitness.append(kmeans.fitnessCosWithDist(dataSet, cluster2, centroid, k))
-        croList.append(cluster2)
-
-        if fitness < max(croFitness):
-            fitness = max(croFitness)
-            newCluster = croList[croFitness.index(max(croFitness))]
-
-        if countIterCRO == 100:
-            f.write('singleMoleculeCollision fitness: ' + str(croFitness[0]) + '\n')
-            f.write('singleMoleculeDecomposition fitness: ' + str(croFitness[1]) + '\n')
-            f.write('intermolecularCollision fitness: ' + str(croFitness[2]) + '\n')
-            f.write('intermolecularSynthesis fitness: ' + str(croFitness[3]) + '\n')
+        if originalCluster == newCluster:
+            sse = kmeans.countSse(euclidDist)
             break
+        else:
+            originalCluster = newCluster
+            centroid = [[kmeans.findCentroid(dataSet, col, cluster, i + 1) for col in range(len(dataSet[0]))]
+                        for i in range(k)]
+
+    f.write('Итоговые центроиды\n')
+    for i in range(len(centroid)):
+        f.write('C' + str(i + 1) + '\t')
+        for j in range(len(centroid[i])):
+            f.write(str(centroid[i][j]) + '\t')
+        f.write('\n')
+    f.write('\n')
+
+    f.write('SSE: ' + str(sse) + '\n\n')
+
+
+    # fitness = kmeans.fitnessCosWithDist(dataSet, originalCluster, centroid, k)
+    # f.write('Fitness: ' + str(fitness) + '\n')
+    # countIterCRO = 0
+    # while True:
+    #     countIterCRO += 1
+    #     croFitness = []
+    #     croList = []
+    #
+    #     # singleMoleculeCollision
+    #     cluster1 = cro.singleMoleculeCollision(len(dataSet), k)
+    #     croFitness.append(kmeans.fitnessCosWithDist(dataSet, cluster1, centroid, k))
+    #     croList.append(cluster1)
+    #
+    #     # singleMoleculeDecomposition
+    #     clusterOdd, clusterEven = cro.singleMoleculeDecomposition(len(dataSet), k)
+    #     fitnessOdd = kmeans.fitnessCosWithDist(dataSet, clusterOdd, centroid, k)
+    #     fitnessEven = kmeans.fitnessCosWithDist(dataSet, clusterEven, centroid, k)
+    #     if fitnessOdd >= fitnessEven:
+    #         croFitness.append(fitnessOdd)
+    #         croList.append(clusterOdd)
+    #     else:
+    #         croFitness.append(fitnessEven)
+    #         croList.append(clusterEven)
+    #
+    #     # intermolecularCollision
+    #     clusterPhi1, clusterPhi2 = cro.intermolecularCollision(len(dataSet), k)
+    #     fitnessPhi1 = kmeans.fitnessCosWithDist(dataSet, clusterPhi1, centroid, k)
+    #     fitnessPhi2 = kmeans.fitnessCosWithDist(dataSet, clusterPhi2, centroid, k)
+    #     if fitnessPhi1 >= fitnessPhi2:
+    #         croFitness.append(fitnessPhi1)
+    #         croList.append(clusterPhi1)
+    #     else:
+    #         croFitness.append(fitnessPhi2)
+    #         croList.append(clusterPhi2)
+    #
+    #     # intermolecularSynthesis
+    #     cluster2 = cro.intermolecularSynthesis(len(dataSet), k)
+    #     croFitness.append(kmeans.fitnessCosWithDist(dataSet, cluster2, centroid, k))
+    #     croList.append(cluster2)
+    #
+    #     if fitness < max(croFitness):
+    #         fitness = max(croFitness)
+    #         newCluster = croList[croFitness.index(max(croFitness))]
+    #
+    #     if countIterCRO == 100:
+    #         f.write('singleMoleculeCollision fitness: ' + str(croFitness[0]) + '\n')
+    #         f.write('singleMoleculeDecomposition fitness: ' + str(croFitness[1]) + '\n')
+    #         f.write('intermolecularCollision fitness: ' + str(croFitness[2]) + '\n')
+    #         f.write('intermolecularSynthesis fitness: ' + str(croFitness[3]) + '\n')
+    #         break
 
     f.write('\n')
     h += 1
 f.close()
+
