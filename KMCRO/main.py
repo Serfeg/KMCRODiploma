694a1f7c144ecebed0aa0db79354dd5a0cbb7a03
Serfeg <mirrorsnight@gmail.com>
2022-03-31 17:56:51 +0700

    19.1 Проверка точности и добавлен выбор файла, чтобы не писать вручную

--- KMCRO/main.py
+++ KMCRO/main.py
@@ -3,13 +3,13 @@ import random as r
 import matplotlib.pyplot as plt
 import kmeans
 import cro
-from scipy.stats.contingency import crosstab
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 
 def makeDataSet(filename):
     dataSet = []
+    eps = []
     file = open(filename)
     s = file.readlines()[1:]
     for i in s:
@@ -19,19 +19,85 @@ def makeDataSet(filename):
         for j in range(len(dataSet[i])):
             dataSet[i][j] = float(dataSet[i][j])
     for i in range(len(dataSet)):
+        eps.append(int(dataSet[i][0]))
+    for i in range(len(dataSet)):
         dataSet[i].pop(0)
-    return dataSet
+    return dataSet, eps
+
+def makeEpsCluster(fileName, eps):
+    epsCluster = []
+    if fileName == '33_42.txt':
+        for i in range(len(eps)):
+            str1 = str(eps[i])[0:2]
+            if str1 == '33':
+                epsCluster.append(1)
+            elif str1 == '42':
+                epsCluster.append(2)
+    elif fileName == '33_37_42.txt':
+        for i in range(len(eps)):
+            str1 = str(eps[i])[0:2]
+            if str1 == '33':
+                epsCluster.append(1)
+            elif str1 == '37':
+                epsCluster.append(2)
+            elif str1 == '42':
+                epsCluster.append(3)
+    elif fileName == '11_33_37_42.txt':
+        for i in range(len(eps)):
+            str1 = str(eps[i])[0:2]
+            if str1 == '11':
+                epsCluster.append(1)
+            elif str1 == '33':
+                epsCluster.append(2)
+            elif str1 == '37':
+                epsCluster.append(3)
+            elif str1 == '42':
+                epsCluster.append(4)
+    elif fileName == 'total_sample_no_empties.txt':
+        for i in range(len(eps)):
+            str1 = str(eps[i])[0:2]
+            if str1 == '11':
+                epsCluster.append(1)
+            elif str1 == '13':
+                epsCluster.append(2)
+            elif str1 == '21':
+                epsCluster.append(3)
+            elif str1 == '33':
+                epsCluster.append(4)
+            elif str1 == '37':
+                epsCluster.append(5)
+            elif str1 == '42':
+                epsCluster.append(6)
+            elif str1 == '45':
+                epsCluster.append(7)
+    return epsCluster
 
 
 if __name__ == "__main__":
-    dataSet = makeDataSet("33_42.txt")
-    # for i in range(len(dataSet)):
-    #     sum = 0
-    #     for j in range(len(dataSet[i])):
-    #         sum += dataSet[i][j] ** 2
-    #     sum = m.sqrt(sum)
-    #     for j in range(len(dataSet[i])):
-    #         dataSet[i][j] *= 1/sum
+    print('Введите номер файла.\n\t'
+          '1. 33_42\n\t'
+          '2. 33_37_42\n\t'
+          '3. 11_33_37_42\n\t'
+          '4. total_sample_no_empties')
+    fileNumber = int(input())
+    if fileNumber == 1:
+        fName = '33_42.txt'
+    elif fileNumber == 2:
+        fName = '33_37_42.txt'
+    elif fileNumber == 3:
+        fName = '11_33_37_42.txt'
+    elif fileNumber == 4:
+        fName = 'total_sample_no_empties.txt'
+    #fName = '33_42.txt'
+    dataSet, eps = makeDataSet(fName)
+    epsCluster = makeEpsCluster(fName, eps)
+    for i in range(len(dataSet)):
+        sum = 0
+        for j in range(len(dataSet[i])):
+            sum += dataSet[i][j] ** 2
+        sum = m.sqrt(sum)
+        for j in range(len(dataSet[i])):
+            dataSet[i][j] *= 1/sum
     #print(min(min(dataSet)), max(max(dataSet)))
     #dataSet = [[dataSet[i][j] / len(dataSet) for j in range(len(dataSet[i]))] for i in range(len(dataSet))]
     df = pd.read_csv('33_42.txt', delimiter="\t")
@@ -47,9 +113,9 @@ if __name__ == "__main__":
     originalCluster = [r.randint(1, k) for i in range(len(dataSet))]
 
     #newCluster, centroid, sse, countIterKMeans = kmeans.kMeans(dataSet, originalCluster, k)
-    #newCluster, centroid, sse, countIterKMeans = kmeans.kMeansWithCos(dataSet, originalCluster, k)
+    newCluster, centroid, sse, countIterKMeans = kmeans.kMeansWithCos(dataSet, originalCluster, k)
     #newCluster, centroid, sse, countIterKMeans = kmeans.kMeansWithCosRandomCentroid(dataSet, originalCluster, k)
-    newCluster, centroid, sse, countIterKMeans = kmeans.kMeansRandom(dataSet, originalCluster, k)
+    #newCluster, centroid, sse, countIterKMeans = kmeans.kMeansRandom(dataSet, originalCluster, k)
 
     dfCentroid = pd.DataFrame()
 
@@ -112,4 +178,5 @@ if __name__ == "__main__":
           f"Fitness CRO: {fitness}\n"
           f"Centroid:\n{dfCentroid}\n\n"
           f"DataSet with Clusters:\n{df}\n\n"
-          f"Confusion Matrix:\n{confusion_matrix(originalCluster, newCluster)}")
+          f"Confusion Matrix:\n{confusion_matrix(epsCluster, newCluster)}")
+
